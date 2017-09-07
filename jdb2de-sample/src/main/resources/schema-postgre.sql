910f440c5b8a280b5c69353ab1bd13bf26c7896e
Rodrigo Tavares <rodrigo@managersys.com.br>
2017-09-07 15:17:09 -0300

    Create table Comments
    Added comments for sample tables

--- jdb2de-sample/src/main/resources/schema-postgre.sql
+++ jdb2de-sample/src/main/resources/schema-postgre.sql
@@ -1,139 +1,183 @@
 CREATE TABLE customer (
-  customer_id INTEGER DEFAULT nextval('customer_customer_id_seq' :: REGCLASS) NOT NULL,
-  store_id    SMALLINT                                                        NOT NULL,
-  first_name  CHARACTER VARYING(45)                                           NOT NULL,
-  last_name   CHARACTER VARYING(45)                                           NOT NULL,
-  email       CHARACTER VARYING(50),
-  address_id  SMALLINT                                                        NOT NULL,
-  activebool  BOOLEAN DEFAULT TRUE                                            NOT NULL,
-  create_date DATE DEFAULT ('now' :: TEXT) :: DATE                            NOT NULL,
-  last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
+  customer_id INTEGER                NOT NULL,
+  store_id    SMALLINT               NOT NULL,
+  first_name  VARCHAR(45)            NOT NULL,
+  last_name   VARCHAR(45)            NOT NULL,
+  email       VARCHAR(50),
+  address_id  SMALLINT               NOT NULL,
+  activebool  BOOLEAN DEFAULT TRUE   NOT NULL,
+  create_date DATE DEFAULT now()     NOT NULL,
+  last_update TIMESTAMP DEFAULT now(),
   active      INTEGER
 );
 
+COMMENT ON TABLE  customer             IS 'Customer registration';
+COMMENT ON COLUMN customer.customer_id IS 'Customer identification';
+COMMENT ON COLUMN customer.store_id    IS 'Store identification';
+COMMENT ON COLUMN customer.first_name  IS 'Customer first name';
+COMMENT ON COLUMN customer.last_name   IS 'Customer last name';
+COMMENT ON COLUMN customer.email       IS 'Customer email address';
+COMMENT ON COLUMN customer.address_id  IS 'Customer address identification';
+COMMENT ON COLUMN customer.activebool  IS 'Indicate if customer is active (boolean)';
+COMMENT ON COLUMN customer.create_date IS 'Creation date';
+COMMENT ON COLUMN customer.last_update IS 'Last update';
+COMMENT ON COLUMN customer.active      IS 'Indicate if customer is active (integer)';
+
 CREATE TABLE actor (
-  actor_id    INTEGER DEFAULT nextval('actor_actor_id_seq' :: REGCLASS) NOT NULL,
-  first_name  CHARACTER VARYING(45)                                     NOT NULL,
-  last_name   CHARACTER VARYING(45)                                     NOT NULL,
-  last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now()                 NOT NULL
+  actor_id    INTEGER                 NOT NULL,
+  first_name  VARCHAR(45)             NOT NULL,
+  last_name   VARCHAR(45)             NOT NULL,
+  last_update TIMESTAMP DEFAULT now() NOT NULL
 );
 
+COMMENT ON TABLE  actor             IS 'Actor list';
+COMMENT ON COLUMN actor.actor_id    IS 'Actor identification';
+COMMENT ON COLUMN actor.first_name  IS 'Actor fist name';
+COMMENT ON COLUMN actor.last_name   IS 'Actor last name';
+COMMENT ON COLUMN actor.last_update IS 'Last update';
 
 CREATE TABLE category (
-  category_id INTEGER DEFAULT nextval('category_category_id_seq' :: REGCLASS) NOT NULL,
-  name        CHARACTER VARYING(25)                                           NOT NULL,
-  last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now()                       NOT NULL
+  category_id INTEGER                 NOT NULL,
+  name        VARCHAR(25)             NOT NULL,
+  last_update TIMESTAMP DEFAULT now() NOT NULL
 );
 
+COMMENT ON TABLE  category             IS 'Movie categories';
+COMMENT ON COLUMN category.category_id IS 'Category identification';
+COMMENT ON COLUMN category.name        IS 'Category name';
+COMMENT ON COLUMN category.last_update IS 'Last update';
 
 CREATE TABLE film (
-  film_id          INTEGER DEFAULT nextval('film_film_id_seq' :: REGCLASS) NOT NULL,
-  title            CHARACTER VARYING(255)                                  NOT NULL,
+  film_id          INTEGER                     NOT NULL,
+  title            VARCHAR(255)                NOT NULL,
   description      TEXT,
-  release_year     YEAR,
-  language_id      SMALLINT                                                NOT NULL,
-  rental_duration  SMALLINT DEFAULT 3                                      NOT NULL,
-  rental_rate      NUMERIC(4, 2) DEFAULT 4.99                              NOT NULL,
+  release_year     SMALLINT,
+  language_id      SMALLINT                    NOT NULL,
+  rental_duration  SMALLINT DEFAULT 3          NOT NULL,
+  rental_rate      NUMERIC(4, 2) DEFAULT 4.99  NOT NULL,
   length           SMALLINT,
-  replacement_cost NUMERIC(5, 2) DEFAULT 19.99                             NOT NULL,
-  rating           MPAA_RATING DEFAULT 'G' :: MPAA_RATING,
-  last_update      TIMESTAMP WITHOUT TIME ZONE DEFAULT now()               NOT NULL,
+  replacement_cost NUMERIC(5, 2) DEFAULT 19.99 NOT NULL,
+  rating           VARCHAR(1) DEFAULT 'G',
+  last_update      TIMESTAMP DEFAULT now()     NOT NULL,
   special_features TEXT [],
-  fulltext         TSVECTOR                                                NOT NULL
+  fulltext         TSVECTOR                    NOT NULL
 );
 
+COMMENT ON TABLE  film                  IS 'Available film';
+COMMENT ON COLUMN film.film_id          IS 'Film identification';
+COMMENT ON COLUMN film.title            IS 'Film title';
+COMMENT ON COLUMN film.description      IS 'Film description';
+COMMENT ON COLUMN film.release_year     IS 'Film release year';
+COMMENT ON COLUMN film.language_id      IS 'Language identification';
+COMMENT ON COLUMN film.rental_duration  IS 'Rental duration';
+COMMENT ON COLUMN film.rental_rate      IS 'Rental rate';
+COMMENT ON COLUMN film.length           IS 'Rental length';
+COMMENT ON COLUMN film.replacement_cost IS 'Replacement cost';
+COMMENT ON COLUMN film.rating           IS 'Rating';
+COMMENT ON COLUMN film.last_update      IS 'Last update';
+COMMENT ON COLUMN film.special_features IS 'Special features';
+COMMENT ON COLUMN film.fulltext         IS 'Full text';
 
 CREATE TABLE film_actor (
-  actor_id    SMALLINT                                  NOT NULL,
-  film_id     SMALLINT                                  NOT NULL,
-  last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL
+  actor_id    SMALLINT                NOT NULL,
+  film_id     SMALLINT                NOT NULL,
+  last_update TIMESTAMP DEFAULT now() NOT NULL
 );
+
+COMMENT ON TABLE  film_actor          IS 'Film and actor relation';
+COMMENT ON COLUMN film_actor.actor_id IS 'Actor identification';
+COMMENT ON COLUMN film_actor.film_id  IS 'Film identification';
+COMMENT ON COLUMN film.last_update    IS 'Last update';
 
 CREATE TABLE film_category (
-  film_id     SMALLINT                                  NOT NULL,
-  category_id SMALLINT                                  NOT NULL,
-  last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL
+  film_id     SMALLINT                NOT NULL,
+  category_id SMALLINT                NOT NULL,
+  last_update TIMESTAMP DEFAULT now() NOT NULL
 );
 
+COMMENT ON TABLE  film_category             IS 'Film and category relation';
+COMMENT ON COLUMN film_category.film_id     IS 'Film identification';
+COMMENT ON COLUMN film_category.category_id IS 'Category identification';
+COMMENT ON COLUMN film_category.last_update IS 'Last update';
 
 CREATE TABLE address (
-  address_id  INTEGER DEFAULT nextval('address_address_id_seq' :: REGCLASS) NOT NULL,
-  address     CHARACTER VARYING(50)                                         NOT NULL,
-  address2    CHARACTER VARYING(50),
-  district    CHARACTER VARYING(20)                                         NOT NULL,
-  city_id     SMALLINT                                                      NOT NULL,
-  postal_code CHARACTER VARYING(10),
-  phone       CHARACTER VARYING(20)                                         NOT NULL,
-  last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now()                     NOT NULL
+  address_id  INTEGER                 NOT NULL,
+  address     VARCHAR(50)             NOT NULL,
+  address2    VARCHAR(50),
+  district    VARCHAR(20)             NOT NULL,
+  city_id     SMALLINT                NOT NULL,
+  postal_code VARCHAR(10),
+  phone       VARCHAR(20)             NOT NULL,
+  last_update TIMESTAMP DEFAULT now() NOT NULL
 );
 
 
 CREATE TABLE city (
-  city_id     INTEGER DEFAULT nextval('city_city_id_seq' :: REGCLASS) NOT NULL,
-  city        CHARACTER VARYING(50)                                   NOT NULL,
-  country_id  SMALLINT                                                NOT NULL,
-  last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now()               NOT NULL
+  city_id     INTEGER                 NOT NULL,
+  city        VARCHAR(50)             NOT NULL,
+  country_id  SMALLINT                NOT NULL,
+  last_update TIMESTAMP DEFAULT now() NOT NULL
 );
 
 CREATE TABLE country (
-  country_id  INTEGER DEFAULT nextval('country_country_id_seq' :: REGCLASS) NOT NULL,
-  country     CHARACTER VARYING(50)                                         NOT NULL,
-  last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now()                     NOT NULL
+  country_id  INTEGER                 NOT NULL,
+  country     VARCHAR(50)             NOT NULL,
+  last_update TIMESTAMP DEFAULT now() NOT NULL
 );
 
 
 CREATE TABLE inventory (
-  inventory_id INTEGER DEFAULT nextval('inventory_inventory_id_seq' :: REGCLASS) NOT NULL,
-  film_id      SMALLINT                                                          NOT NULL,
-  store_id     SMALLINT                                                          NOT NULL,
-  last_update  TIMESTAMP WITHOUT TIME ZONE DEFAULT now()                         NOT NULL
+  inventory_id INTEGER                 NOT NULL,
+  film_id      SMALLINT                NOT NULL,
+  store_id     SMALLINT                NOT NULL,
+  last_update  TIMESTAMP DEFAULT now() NOT NULL
 );
 
 
 CREATE TABLE language (
-  language_id INTEGER DEFAULT nextval('language_language_id_seq' :: REGCLASS) NOT NULL,
-  name        CHARACTER(20)                                                   NOT NULL,
-  last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now()                       NOT NULL
+  language_id INTEGER                 NOT NULL,
+  name        CHARACTER(20)           NOT NULL,
+  last_update TIMESTAMP DEFAULT now() NOT NULL
 );
 
 CREATE TABLE payment (
-  payment_id   INTEGER DEFAULT nextval('payment_payment_id_seq' :: REGCLASS) NOT NULL,
-  customer_id  SMALLINT                                                      NOT NULL,
-  staff_id     SMALLINT                                                      NOT NULL,
-  rental_id    INTEGER                                                       NOT NULL,
-  amount       NUMERIC(5, 2)                                                 NOT NULL,
-  payment_date TIMESTAMP WITHOUT TIME ZONE                                   NOT NULL
+  payment_id   INTEGER       NOT NULL,
+  customer_id  SMALLINT      NOT NULL,
+  staff_id     SMALLINT      NOT NULL,
+  rental_id    INTEGER       NOT NULL,
+  amount       NUMERIC(5, 2) NOT NULL,
+  payment_date TIMESTAMP     NOT NULL
 );
 
 CREATE TABLE rental (
-  rental_id    INTEGER DEFAULT nextval('rental_rental_id_seq' :: REGCLASS) NOT NULL,
-  rental_date  TIMESTAMP WITHOUT TIME ZONE                                 NOT NULL,
-  inventory_id INTEGER                                                     NOT NULL,
-  customer_id  SMALLINT                                                    NOT NULL,
-  return_date  TIMESTAMP WITHOUT TIME ZONE,
-  staff_id     SMALLINT                                                    NOT NULL,
-  last_update  TIMESTAMP WITHOUT TIME ZONE DEFAULT now()                   NOT NULL
+  rental_id    INTEGER                 NOT NULL,
+  rental_date  TIMESTAMP               NOT NULL,
+  inventory_id INTEGER                 NOT NULL,
+  customer_id  SMALLINT                NOT NULL,
+  return_date  TIMESTAMP,
+  staff_id     SMALLINT                NOT NULL,
+  last_update  TIMESTAMP DEFAULT now() NOT NULL
 );
 
 CREATE TABLE staff (
-  staff_id    INTEGER DEFAULT nextval('staff_staff_id_seq' :: REGCLASS) NOT NULL,
-  first_name  CHARACTER VARYING(45)                                     NOT NULL,
-  last_name   CHARACTER VARYING(45)                                     NOT NULL,
-  address_id  SMALLINT                                                  NOT NULL,
-  email       CHARACTER VARYING(50),
-  store_id    SMALLINT                                                  NOT NULL,
-  active      BOOLEAN DEFAULT TRUE                                      NOT NULL,
-  username    CHARACTER VARYING(16)                                     NOT NULL,
-  password    CHARACTER VARYING(40),
-  last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now()                 NOT NULL,
+  staff_id    INTEGER                 NOT NULL,
+  first_name  VARCHAR(45)             NOT NULL,
+  last_name   VARCHAR(45)             NOT NULL,
+  address_id  SMALLINT                NOT NULL,
+  email       VARCHAR(50),
+  store_id    SMALLINT                NOT NULL,
+  active      BOOLEAN DEFAULT TRUE    NOT NULL,
+  username    VARCHAR(16)             NOT NULL,
+  password    VARCHAR(40),
+  last_update TIMESTAMP DEFAULT now() NOT NULL,
   picture     BYTEA
 );
 
 CREATE TABLE store (
-  store_id         INTEGER DEFAULT nextval('store_store_id_seq' :: REGCLASS) NOT NULL,
-  manager_staff_id SMALLINT                                                  NOT NULL,
-  address_id       SMALLINT                                                  NOT NULL,
-  last_update      TIMESTAMP WITHOUT TIME ZONE DEFAULT now()                 NOT NULL
+  store_id         INTEGER                 NOT NULL,
+  manager_staff_id SMALLINT                NOT NULL,
+  address_id       SMALLINT                NOT NULL,
+  last_update      TIMESTAMP DEFAULT now() NOT NULL
 );
 
 -- #################################
